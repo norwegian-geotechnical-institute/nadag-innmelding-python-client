b3b2f967e0a0c77f6410531d9ab5190e04befde0
Jostein Leira <9363084+josteinl@users.noreply.github.com>
2025-10-08 10:06:47 +0200

    - manually patch the OpenAPI specification for `POST /nadag/innmelding/v1/GeotekniskBorehull` with content type from `*/*` (unsupported content type) to `application/json`. - manually compile new package

--- nadag-innmelding-python-client/nadag_innmelding_python_client/models/epsg_code.py
+++ nadag-innmelding-python-client/nadag_innmelding_python_client/models/epsg_code.py
@@ -2,11 +2,12 @@ from enum import Enum
 
 
 class EpsgCode(str, Enum):
-    EPSG_32632 = "epsg_32632"
-    EPSG_32633 = "epsg_32633"
-    EPSG_32634 = "epsg_32634"
-    EPSG_32635 = "epsg_32635"
-    EPSG_32636 = "epsg_32636"
+    EPSG_25831 = "epsg_25831"
+    EPSG_25832 = "epsg_25832"
+    EPSG_25833 = "epsg_25833"
+    EPSG_25834 = "epsg_25834"
+    EPSG_25835 = "epsg_25835"
+    EPSG_25836 = "epsg_25836"
     EPSG_4326 = "epsg_4326"
     EPSG_5105 = "epsg_5105"
     EPSG_5106 = "epsg_5106"
@@ -34,6 +35,7 @@ class EpsgCode(str, Enum):
     EPSG_5128 = "epsg_5128"
     EPSG_5129 = "epsg_5129"
     EPSG_5130 = "epsg_5130"
+    EPSG_5971 = "epsg_5971"
     EPSG_5972 = "epsg_5972"
     EPSG_5973 = "epsg_5973"
     EPSG_5974 = "epsg_5974"
